fa52dcc9fde5db638805e6db28eb3b7462402701
Lancelin <78137422+m2gi-foussadl@users.noreply.github.com>
2025-04-06 20:39:38 +0200

    Ajout de la gestion des flèches pour se déplacer dans le buffer

--- step1/main.c
+++ step1/main.c
@@ -30,13 +30,85 @@ void check_stacks() {
     panic();
 */
 }
-
+size_t strlen(const char *str) {
+    size_t len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+void exit(char status) {
+    // Code pour quitter le programme
+    // On va juste boucler ici pour simuler un exit
+    // On peut éventuellement afficher le code de sortie
+    uart_send_string(UART0, "Ctrl-C pressed, exiting with status: ");
+    uart_send(UART0, status);
+    uart_send_string(UART0, "\n");
+    uart_disable(UART0);
+    uart_disable(UART1);
+    uart_disable(UART2);
+    while (1) {
+    }
+}
 void uart_handler(uint32_t irq, void *cookie) {
+    static char buffer[256];
+    static int pos = 0;
+    static int escape_seq = 0;
     char c;
     uart_receive(UART0, &c);
-    uart_send_string(UART0, "Received: ");
-    uart_send(UART0, c);
-    uart_send_string(UART0, "\n");
+
+    if (escape_seq == 1) {
+        if (c == '[') {
+            escape_seq = 2;
+        } else {
+            escape_seq = 0;
+        }
+        return;
+    } else if (escape_seq == 2) {
+        if (c == 'D') {
+            // Flèche gauche
+            if (pos > 0) {
+                pos--;
+                uart_send_string(UART0, "\x1B[D");
+            }
+        } else if (c == 'C') {
+            // Flèche droite
+            if (pos < strlen(buffer)) {
+                pos++;
+                uart_send_string(UART0, "\x1B[C");
+            }
+        }
+        escape_seq = 0;
+        return;
+    }
+
+    if (c == 0x1B) {
+        escape_seq = 1;
+        return;
+    }
+
+    // Si c'est Ctrl-C, exit le programme
+    if (c == 0x03) {
+        exit('0');
+
+    }
+    //Si c'est un retour chariot, on termine la ligne
+    // Si c'est un retour chariot, on passe à la ligne suivante
+    // Si c'est un retour chariot, on passe à la ligne suivante
+    // Si c'est un retour chariot, on passe à la ligne suivante
+    if (c == '\r' || c == '\n') {
+        uart_send_string(UART0, "\n");
+        pos = 0;
+        buffer[0] = '\0'; // Réinitialiser le buffer
+        return;
+    }
+
+    // Sinon, on ajoute le caractère au buffer et on l'affiche
+    if (pos < sizeof(buffer) - 1) {
+        buffer[pos++] = c;
+        buffer[pos] = '\0';
+        uart_send(UART0, c);
+    }
 }
 /**
  * This is the C entry point,
